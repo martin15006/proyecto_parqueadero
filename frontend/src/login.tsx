36adcd9dd486f18230a52cbed4fc25a8f972b823
Joshua Dominguez <joshuajuliandominguez@gmail.com>
2026-05-06 06:07:48 -0500

    Base Login

---
+++ frontend/src/login.tsx
@@ -0,0 +1,17 @@
+function Login() {
+    return (
+    <>
+        <h1>Login</h1>
+
+        <form>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" />
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+            <button type="submit">Login</button>
+        </form>
+    </>
+    )
+}
+
+export default Login
